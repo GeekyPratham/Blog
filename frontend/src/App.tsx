df3c1b594ee0b2d7b1b3e2d9611a9b3b11fc3f70
GeekyPratham <kprathamraj2021@gmail.com>
2025-06-25 02:07:37 +0530

    fixing errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ const App = () => {
   return (
     <BrowserRouter> 
       <Routes>
+        <Route path="/" element={<Signin />} />
         <Route path="/blog/*" element={<Blog />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
