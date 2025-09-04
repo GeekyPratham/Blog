31759aa22901f176e45d7b8c61cbc6b9288703b7
Pratham Raj <146612404+GeekyPratham@users.noreply.github.com>
2025-09-04 16:33:38 +0530

    Update Quote.tsx

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -4,10 +4,10 @@ const Quote = () => {
   return (
     <div className="max-w-md text-left">
       <p className="text-xl sm:text-2xl font-semibold text-white mb-4">
-        “The customer service I received was exceptional. The support team went above and beyond to address my concerns.“
+        “The beautiful thing about learning is that no one can take it away from you“
       </p>
-      <p className="font-bold text-green-300">Jules Winnfield</p>
-      <p className="text-sm text-gray-300">CEO, Acme Inc</p>
+      <p className="font-bold text-green-300">B.B. King</p>
+      
     </div>
   );
 };
